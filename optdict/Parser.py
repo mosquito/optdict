085ac013b90a92d7798487dbf7f93f541480ae68
Dmitry Orlov <dorlov@undev.ru>
2013-07-31 14:25:21 +0400

    unuse import

--- optdict/Parser.py
+++ optdict/Parser.py
@@ -9,7 +9,6 @@
 """
 
 from __future__ import print_function, absolute_import
-from inspect import isfunction
 from optparse import OptionParser, OptionGroup, OptionValueError, OptionConflictError
 import sys
 import codecs
